ea35cae39a1f6e92b120a55e6010894efdb1a1ab
WhiteFox <53935741+WhiteFox2232@users.noreply.github.com>
2022-07-06 15:51:11 +0200

    💎 GamemodeCommand V2

--- src/main/java/fr/whitefox/hera/commands/GamemodeCommand.java
+++ src/main/java/fr/whitefox/hera/commands/GamemodeCommand.java
@@ -1,5 +1,6 @@
 package fr.whitefox.hera.commands;
 
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.GameMode;
 import org.bukkit.command.Command;
@@ -20,20 +21,72 @@ public class GamemodeCommand implements CommandExecutor {
                     player.sendMessage(ChatColor.RED + "Vous devez spécifier un mode de jeu ! Exemple : /gm <0;1;2;3>");
 
                 } else if (args[0].equalsIgnoreCase("0")) {
-                    player.setGameMode(GameMode.SURVIVAL);
-                    player.sendMessage("§6[§9Hera§6] §aVotre mode de jeu a bien été défini sur §2§lSurvival");
+                    if(args.length == 1){
+                        player.setGameMode(GameMode.SURVIVAL);
+                        player.sendMessage("§6[§9Hera§6] §aVotre mode de jeu a bien été défini sur §2§lSurvival");
+                    } else{
+                        Player target = Bukkit.getServer().getPlayer(args[1]);
+
+                        if (target == null) {
+                            player.sendMessage(ChatColor.RED + "Le joueur n'existe pas ou n'est pas connecté !");
+                            return true;
+                        }
+
+                        target.setGameMode(GameMode.SURVIVAL);
+                        player.sendMessage("§6[§9Hera§6] §aLe mode de jeu de " + target.getName() + " a bien été défini sur §2§lSurvival");
+                        target.sendMessage("§6[§9Hera§6] §aVotre mode de jeu a bien été défini sur §2§lSurvival");
+                    }
 
                 } else if (args[0].equalsIgnoreCase("1")) {
-                    player.setGameMode(GameMode.CREATIVE);
-                    player.sendMessage("§6[§9Hera§6] §aVotre mode de jeu a bien été défini sur §2§lCréatif");
+                    if(args.length == 1){
+                        player.setGameMode(GameMode.CREATIVE);
+                        player.sendMessage("§6[§9Hera§6] §aVotre mode de jeu a bien été défini sur §2§lCréatif");
+                    } else{
+                        Player target = Bukkit.getServer().getPlayer(args[1]);
+
+                        if (target == null) {
+                            player.sendMessage(ChatColor.RED + "Le joueur n'existe pas ou n'est pas connecté !");
+                            return true;
+                        }
+
+                        target.setGameMode(GameMode.CREATIVE);
+                        player.sendMessage("§6[§9Hera§6] §aLe mode de jeu de " + target.getName() + " a bien été défini sur §2§lCréatif");
+                        target.sendMessage("§6[§9Hera§6] §aVotre mode de jeu a bien été défini sur §2§lCréatif");
+                    }
 
                 } else if (args[0].equalsIgnoreCase("2")) {
-                    player.setGameMode(GameMode.ADVENTURE);
-                    player.sendMessage("§6[§9Hera§6] §aVotre mode de jeu a bien été défini sur §2§lAventure");
+                    if(args.length == 1){
+                        player.setGameMode(GameMode.ADVENTURE);
+                        player.sendMessage("§6[§9Hera§6] §aVotre mode de jeu a bien été défini sur §2§lAventure");
+                    } else{
+                        Player target = Bukkit.getServer().getPlayer(args[1]);
+
+                        if (target == null) {
+                            player.sendMessage(ChatColor.RED + "Le joueur n'existe pas ou n'est pas connecté !");
+                            return true;
+                        }
+
+                        target.setGameMode(GameMode.ADVENTURE);
+                        player.sendMessage("§6[§9Hera§6] §aLe mode de jeu de " + target.getName() + " a bien été défini sur §2§lAventure");
+                        target.sendMessage("§6[§9Hera§6] §aVotre mode de jeu a bien été défini sur §2§lAventure");
+                    }
 
                 } else if (args[0].equalsIgnoreCase("3")) {
-                    player.setGameMode(GameMode.SPECTATOR);
-                    player.sendMessage("§6[§9Hera§6] §aVotre mode de jeu a bien été défini sur §2§lSpectateur");
+                    if(args.length == 1){
+                        player.setGameMode(GameMode.SPECTATOR);
+                        player.sendMessage("§6[§9Hera§6] §aVotre mode de jeu a bien été défini sur §2§lSpectateur");
+                    } else{
+                        Player target = Bukkit.getServer().getPlayer(args[1]);
+
+                        if (target == null) {
+                            player.sendMessage(ChatColor.RED + "Le joueur n'existe pas ou n'est pas connecté !");
+                            return true;
+                        }
+
+                        target.setGameMode(GameMode.SPECTATOR);
+                        player.sendMessage("§6[§9Hera§6] §aLe mode de jeu de " + target.getName() + " a bien été défini sur §2§lSpectateur");
+                        target.sendMessage("§6[§9Hera§6] §aVotre mode de jeu a bien été défini sur §2§lSpectateur");
+                    }
 
                 } else {
                     player.sendMessage(ChatColor.RED + "Vous devez spécifier un mode de jeu valide ! Exemple : /gm <0;1;2;3>");
