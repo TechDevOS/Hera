cc8cbe1c51fc3528b2878a4cc10bd4c52cf9cee0
WhiteFox <53935741+WhiteFox2232@users.noreply.github.com>
2022-07-07 22:37:02 +0200

    ✨Heal and Feed for other players

--- src/main/java/fr/whitefox/hera/commands/FeedCommand.java
+++ src/main/java/fr/whitefox/hera/commands/FeedCommand.java
@@ -1,9 +1,14 @@
 package fr.whitefox.hera.commands;
 
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.GameMode;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
 
 public class FeedCommand implements CommandExecutor {
 
@@ -17,12 +22,24 @@ public class FeedCommand implements CommandExecutor {
                 if (args.length == 0) {
                     player.setFoodLevel(20);
                     player.sendMessage("§6[§9Hera§6] §aVotre nourriture vient d'être restaurée !");
+                }
 
+                if (args.length > 0){
+                    Player target = Bukkit.getServer().getPlayer(args[0]);
+
+                    if (target == null) {
+                        player.sendMessage(ChatColor.RED + "Le joueur n'existe pas ou n'est pas connecté !");
+                        return true;
+                    }
+                    target.setFoodLevel(20);
+                    target.sendMessage("§6[§9Hera§6] §aVotre nourriture vient d'être restaurée !");
+                    player.sendMessage("§6[§9Hera§6] §aVous venez de restaurer la nourriture de §c" + target.getName() + "§a !");
+
+                    return true;
                 }
 
                 return true;
             }
-
         }
         return true;
     }
--- src/main/java/fr/whitefox/hera/commands/HealCommand.java
+++ src/main/java/fr/whitefox/hera/commands/HealCommand.java
@@ -1,5 +1,7 @@
 package fr.whitefox.hera.commands;
 
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -16,12 +18,24 @@ public class HealCommand implements CommandExecutor {
                 if (args.length == 0) {
                     player.setHealth(20.0);
                     player.sendMessage("§6[§9Hera§6] §aVotre santé vient d'être restaurée !");
+                }
 
+                if (args.length > 0){
+                    Player target = Bukkit.getServer().getPlayer(args[0]);
+
+                    if (target == null) {
+                        player.sendMessage(ChatColor.RED + "Le joueur n'existe pas ou n'est pas connecté !");
+                        return true;
+                    }
+                    target.setHealth(20.0);
+                    target.sendMessage("§6[§9Hera§6] §aVotre santé vient d'être restaurée !");
+                    player.sendMessage("§6[§9Hera§6] §aVous venez de restaurer la santé de §c" + target.getName() + "§a !");
+
+                    return true;
                 }
 
                 return true;
             }
-
         }
         return true;
     }
