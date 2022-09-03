8fa77bc77f5693797fa75bc3443ba441bc113403
WhiteFox <53935741+WhiteFox2232@users.noreply.github.com>
2022-09-03 18:56:59 +0200

    🚀 New chat mention feature

--- src/main/java/fr/whitefox/hera/events/PlayerChat.java
+++ src/main/java/fr/whitefox/hera/events/PlayerChat.java
@@ -1,5 +1,6 @@
 package fr.whitefox.hera.events;
 
+import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -8,9 +9,19 @@ import org.bukkit.event.player.AsyncPlayerChatEvent;
 public class PlayerChat implements Listener {
 
     @EventHandler
-    public void onPlayerChat(AsyncPlayerChatEvent event){
+    public void onPlayerMention(AsyncPlayerChatEvent event){
         Player player = event.getPlayer();
         String message = String.format(event.getMessage());
 
+        event.setCancelled(true);
+
+        for (Player people : Bukkit.getServer().getOnlinePlayers()) {
+            if(message.contains(people.getName())){
+                String formatedMessage = message.replaceAll(people.getName(), "§5§l" + people.getName() + "§7");
+                people.sendMessage("§7" + player.getName() + " : " + formatedMessage);
+            } else{
+                people.sendMessage("§7" + player.getName() + " : " + message);
+            }
+        }
     }
 }
