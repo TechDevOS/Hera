e29cdf21b9dcdf2a52c9fe8483e7a43975e9cdaf
WhiteFox <53935741+WhiteFox2232@users.noreply.github.com>
2022-09-07 23:23:05 +0200

    🚀 New personal time (pday/pnight) command

--- src/main/java/fr/whitefox/hera/commands/TimeCommand.java
+++ src/main/java/fr/whitefox/hera/commands/TimeCommand.java
@@ -1,5 +1,6 @@
 package fr.whitefox.hera.commands;
 
+import fr.whitefox.hera.Main;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
@@ -10,6 +11,12 @@ import org.bukkit.entity.Player;
 import static org.bukkit.Bukkit.getServer;
 
 public class TimeCommand implements CommandExecutor {
+
+    Main plugin;
+
+    public TimeCommand(Main plugin) {
+        this.plugin = plugin;
+    }
 
     @Override
     public boolean onCommand(CommandSender sender, Command cmd, String msg, String[] args) {
@@ -39,6 +46,47 @@ public class TimeCommand implements CommandExecutor {
             }
         }
 
+        if (cmd.getName().equalsIgnoreCase("pday")) {
+
+            if (sender instanceof Player) {
+                Player player = (Player) sender;
+
+                if (!plugin.pday_list.contains(player)) {
+                    plugin.pday_list.add(player);
+                    plugin.pnight_list.remove(player);
+                    player.setPlayerTime(6000, true);
+                    player.sendMessage("§6[§9Hera§6] §aVotre vision personnelle a bien été définie sur §2§lJour ");
+                } else {
+                    plugin.pday_list.remove(player);
+                    player.resetPlayerTime();
+                    player.sendMessage("§6[§9Hera§6] §aVotre vision personnelle est bien revenue sur celle du serveur. ");
+                }
+
+            } else {
+                if (args.length == 0) {
+                    getServer().getConsoleSender().sendMessage(ChatColor.RED + "Vous devez spécifier un joueur !");
+                    return true;
+                }
+
+                Player target = Bukkit.getServer().getPlayer(args[0]);
+
+                if (target == null) {
+                    getServer().getConsoleSender().sendMessage(ChatColor.RED + "Le joueur n'existe pas ou n'est pas connecté !");
+                    return true;
+                }
+
+                if (!plugin.pday_list.contains(target)) {
+                    plugin.pday_list.add(target);
+                    target.setPlayerTime(6000, true);
+                    getServer().getConsoleSender().sendMessage(ChatColor.GREEN + "La vision personnelle de §6" + target.getName() + "§a a bien été définie sur Jour");
+                } else {
+                    plugin.pday_list.remove(target);
+                    target.resetPlayerTime();
+                    getServer().getConsoleSender().sendMessage(ChatColor.GREEN + "La vision personnelle de §6" + target.getName() + "§a est bien revenue sur celle du serveur.");
+                }
+            }
+        }
+
         if (cmd.getName().equalsIgnoreCase("night")) {
 
             if (sender instanceof Player) {
@@ -63,6 +111,47 @@ public class TimeCommand implements CommandExecutor {
                 }
 
                 return true;
+            }
+        }
+
+        if (cmd.getName().equalsIgnoreCase("pnight")) {
+
+            if (sender instanceof Player) {
+                Player player = (Player) sender;
+
+                if (!plugin.pnight_list.contains(player)) {
+                    plugin.pnight_list.add(player);
+                    plugin.pday_list.remove(player);
+                    player.getLocation().getWorld().setTime(18000);
+                    player.sendMessage(" §aVotre vision personnelle a bien été défini sur §2§lNuit");
+                } else {
+                    plugin.pnight_list.remove(player);
+                    player.resetPlayerTime();
+                    player.sendMessage("§6[§9Hera§6] §aVotre vision personnelle est bien revenue sur celle du serveur. ");
+                }
+
+            } else {
+                if (args.length == 0) {
+                    getServer().getConsoleSender().sendMessage(ChatColor.RED + "Vous devez spécifier un joueur !");
+                    return true;
+                }
+
+                Player target = Bukkit.getServer().getPlayer(args[0]);
+
+                if (target == null) {
+                    getServer().getConsoleSender().sendMessage(ChatColor.RED + "Le joueur n'existe pas ou n'est pas connecté !");
+                    return true;
+                }
+
+                if (!plugin.pnight_list.contains(target)) {
+                    plugin.pnight_list.add(target);
+                    target.getLocation().getWorld().setTime(18000);
+                    getServer().getConsoleSender().sendMessage(ChatColor.GREEN + "La vision personnelle de §6" + target.getName() + "§a a bien été définie sur Nuit");
+                } else {
+                    plugin.pnight_list.remove(target);
+                    target.resetPlayerTime();
+                    getServer().getConsoleSender().sendMessage(ChatColor.GREEN + "La vision personnelle de §6" + target.getName() + "§a est bien revenue sur celle du serveur.");
+                }
             }
         }
 
